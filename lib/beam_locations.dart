de89872039604f809b4f9fb9c23dd9974800d217
Ricardo Morales Domínguez <rmorales@metrofflex.com>
2022-10-30 10:57:55 -0500

    Navigation between B and its details page

--- lib/beam_locations.dart
+++ lib/beam_locations.dart
@@ -26,3 +26,27 @@ class ALocation extends BeamLocation<BeamState> {
   @override
   List<Pattern> get pathPatterns => ['/*'];
 }
+
+class BLocation extends BeamLocation<BeamState> {
+  BLocation(super.routeInformation);
+  @override
+  List<BeamPage> buildPages(BuildContext context, BeamState state) {
+    return [
+      const BeamPage(
+        child: RootScreen(
+          label: 'B',
+          detailsPath: '/b/details',
+        ),
+        type: BeamPageType.fadeTransition,
+      ),
+      if (state.uri.pathSegments.length == 2)
+        const BeamPage(
+          child: DetailsScreen(label: 'B'),
+          type: BeamPageType.scaleTransition,
+        ),
+    ];
+  }
+
+  @override
+  List<Pattern> get pathPatterns => ['/*'];
+}
