ad2a62d958b592b6d76f753635aaeb702b84bf8e
Ricardo Morales Domínguez <rmorales@metrofflex.com>
2022-10-30 10:00:00 -0500

    Building UI for scaffold witn bottom navigation bar

---
+++ lib/scaffold_with_botton_navbar.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class ScaffoldWithBottomNavBar extends StatefulWidget {
+  const ScaffoldWithBottomNavBar({Key? key}) : super(key: key);
+
+  @override
+  State<ScaffoldWithBottomNavBar> createState() =>
+      _ScaffoldWithBottomNavBarState();
+}
+
+class _ScaffoldWithBottomNavBarState extends State<ScaffoldWithBottomNavBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const Center(
+        child: Text('under build'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Section A'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.settings), label: 'Section B')
+        ],
+        onTap: (value) {},
+      ),
+    );
+  }
+}
